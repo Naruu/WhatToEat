ae74bd8db2e16d21c50516e39fe94ee4f8d73d36
Naruu <esara2021@gmail.com>
2019-01-07 05:37:10 +0900

    have drop down form But selected option is not transmitted

--- webpage/eatwhat/views.py
+++ webpage/eatwhat/views.py
@@ -1,26 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from .models import SeasonalIngredients
-
+from .forms import selectForm
 
 # Create your views here.
+app_name = 'eatwhat'
 
-def main_page(request, month=12) :
-    food_list = SeasonalIngredients.objects.filter(month = month)
-    context = {
-        'food_list' : food_list,
-    }
+def main_page(request, month=12, food_list = []) :
+    print("posting")
+    form = selectForm(request.GET)
 
-    return render(request, 'eatwhat/main_page.html', context)
-    ##form = selectForm()
-    ##return HttpResponse("main_page.html", {'form' : form})
+    month = request.GET.get('month',12)
+    print("this is month %d" %month)
 
-"""
-    model = SeasonalIngredients
-    template_name = 'main_page.html'
-    
-    <form method = "POST" class ='main_page'>
-    {% csrf_token%}
-    {{form.as_p}}
-</form>
-"""
+
+    food_list = SeasonalIngredients.objects.filter(month=month)
+
+
+    return render(request, 'eatwhat/main_page.html',{'form' : form, 'month' : month, 'food_list' : food_list})
+
